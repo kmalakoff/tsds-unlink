e805dcde6e0244b5a4c51dd80d3a8a0a4568d05e
Kevin Malakoff <kmalakoff@gmail.com>
2026-05-26 14:42:15 -0700

    Fix broken types #3

--- test/unit/lib.test.ts
+++ test/unit/lib.test.ts
@@ -22,7 +22,7 @@ const __dirname = path.dirname(typeof __filename !== 'undefined' ? __filename : 
 
 const GITS = ['https://github.com/kmalakoff/fetch-http-message.git'];
 
-function addTests(repo) {
+function addTests(repo: string) {
   const repoName = path.basename(repo, path.extname(repo));
   describe(repoName, () => {
     const dest = path.join(tmpdir(), 'tsds-unlink', shortHash(process.cwd()), repoName);
@@ -32,40 +32,30 @@ function addTests(repo) {
     const deps = { ...(modulePackage.dependencies || {}), ...(modulePackage.peerDependencies || {}) };
 
     before((cb) => {
-      installGitRepo(repo, dest, (err?: Error): void => {
-        if (err) {
-          cb(err);
-          return;
-        }
+      installGitRepo(repo, dest, (err?: Error | null): void => {
+        if (err) return cb(err);
 
         const queue = new Queue();
-        queue.defer(linkModule.bind(null, modulePath, nodeModules));
-        for (const dep in deps) queue.defer(linkModule.bind(null, path.dirname(resolveSync(`${dep}/package.json`)), nodeModules));
+        queue.defer((cb) => linkModule(modulePath, nodeModules, (err) => cb(err)));
+        for (const dep in deps) queue.defer((cb) => linkModule(path.dirname(resolveSync(`${dep}/package.json`)), nodeModules, (err) => cb(err)));
         queue.await(cb);
       });
     });
     after((cb) => {
       const queue = new Queue();
-      queue.defer(unlinkModule.bind(null, modulePath, nodeModules));
-      for (const dep in deps) queue.defer(unlinkModule.bind(null, path.dirname(resolveSync(`${dep}/package.json`)), nodeModules));
+      queue.defer((cb) => unlinkModule(modulePath, nodeModules, (err) => cb(err)));
+      for (const dep in deps) queue.defer((cb) => unlinkModule(path.dirname(resolveSync(`${dep}/package.json`)), nodeModules, (err) => cb(err)));
       queue.await(cb);
     });
 
     describe('happy path', () => {
       it('link then unlink', (done) => {
         // First link
-        link([], { cwd: dest }, (err?: Error): void => {
-          if (err) {
-            done(err.message);
-            return;
-          }
+        link([], { cwd: dest }, (err?: Error | null): void => {
+          if (err) return done(err.message);
           // Then unlink
-          unlink([], { cwd: dest }, (err?: Error): void => {
-            if (err) {
-              done(err.message);
-              return;
-            }
-            done();
+          unlink([], { cwd: dest }, (err?: Error | null): void => {
+            err ? done(err) : done();
           });
         });
       });
